9ba1bdd9d1ea067bcee3fab011e0c58ca91d940b
Saleem Lawal <slawal1@umbc.edu>
2026-05-11 00:05:44 -0400

    Add error handling and health check functionality to API, including JSON response utilities

---
+++ backend/cmd/api/errors.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Errorw("Internal server error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+	writeJSONError(w, http.StatusInternalServerError, "The server encountered a problem and could not process your request")
+}
---
+++ backend/cmd/api/health.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+)
+
+// health godoc
+//
+//	@Summary		Health check
+//	@Description	Check the health of the server
+//	@Tags			health
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	map[string]string
+//	@Router			/health [get]
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":  "ok",
+		"env":     app.config.env,
+		"version": version,
+	}
+
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
